a03809525d56ac4aef624e976de77511a617ff44
ramd9630 <ramd9630@gmail.com>
2020-12-19 21:27:50 +0530

    commiting first file into the repo

--- src/com/day1/practice/TestCase1.java
+++ src/com/day1/practice/TestCase1.java
@@ -5,5 +5,6 @@ public class TestCase1 {
     public static void main(String[] args){
         System.out.println("Yelagelaga...");
         System.out.println("Yelagelaga...");
+        System.out.println("Yelagelaga...");
     }
 }
